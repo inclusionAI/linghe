64254529d99e4d77ec38fa0a5164a767a5c279b4
nanxiao.zy <nanxiao.zy@antgroup.com>
2026-03-27 10:55:53 +0800

    WIP tune gemm block size

--- linghe/infer/gemm.py
+++ linghe/infer/gemm.py
@@ -8,16 +8,6 @@ import triton.language as tl
 
 
 
-# fp32_gemm_configs = [
-#     triton.Config({"BLOCK_SIZE_M": block_m,
-#                    "BLOCK_SIZE_N": block_n}, 
-#                    num_stages=3, 
-#                    num_warps=num_warps)
-#     for block_m in [16, 32, 64, 128]
-#     for block_n in [32, 64, 128]
-#     for num_warps in [2, 4]
-# ]
-# @triton.autotune(configs=fp32_gemm_configs, key=["M", "N", "K", "SPLIT_COUNT"])
 @triton.jit
 def split_fp32_gemm_kernel(
         a_ptr,
@@ -78,23 +68,24 @@ def triton_split_fp32_gemm(x: torch.Tensor, w: torch.Tensor):
     M, K = x.size()
     N, K = w.size()
 
-    if M * N >= 128 * 128 * 128:
-        SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 64
-        BLOCK_SIZE_N = 128
-    elif M * N >= 32 * 128 * 128:
-        SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 32
-        BLOCK_SIZE_N = 128
-    elif M * N >= 16 * 64 * 128:
-        SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 16
-        BLOCK_SIZE_N = 64
-    else:
-        SPLIT_COUNT = 4
-        BLOCK_SIZE_M = 16
-        BLOCK_SIZE_N = 32
+    MP = triton.next_power_of_2(M)
+    BLOCK_SIZE_M = max(min(MP, 64), 16)
 
+    if BLOCK_SIZE_M == 16:
+        if N <= 32 * 128:
+            SPLIT_COUNT = 4
+            BLOCK_SIZE_N = 32
+        else:
+            SPLIT_COUNT = 1
+            BLOCK_SIZE_N = 32
+    elif M >= 256 and N % 128 == 0:
+        SPLIT_COUNT = 1
+        BLOCK_SIZE_N = 128
+    else:
+        SPLIT_COUNT = 1
+        BLOCK_SIZE_N = 64
+
+    assert N % BLOCK_SIZE_N == 0
     BLOCK_SIZE_K = 128
     
     if SPLIT_COUNT == 1:
@@ -178,9 +169,22 @@ def triton_tile_block_fp8_gemm(a: torch.Tensor,
     N = b.size(0)
     c = torch.empty(M, N, dtype=out_dtype, device=a.device)
     stride_a_scale = a_s.stride(1)
-    BLOCK_SIZE_M = 16
-    BLOCK_SIZE_N = 32
+
+    MP = triton.next_power_of_2(M)
+    BLOCK_SIZE_M = max(min(MP, 64), 16)
+    
+    if BLOCK_SIZE_M == 16:
+        if N <= 16 * 256:
+            BLOCK_SIZE_N = 16
+        else:
+            BLOCK_SIZE_N = 32
+        num_warps = 2
+    else:
+        BLOCK_SIZE_N = 128
+        num_warps = 4
     BLOCK_SIZE_K = 128
+
+
     grid = (triton.cdiv(M, BLOCK_SIZE_M),
             triton.cdiv(N, BLOCK_SIZE_N))
     num_stages = 3
@@ -268,26 +272,27 @@ def triton_split_tile_block_fp8_gemm(a: torch.Tensor,
     N = b.size(0)
     stride_a_scale = a_s.stride(1)
     TRANSPOSE_A_SCALE = not a_s.is_contiguous()
-    if M * N >= 64 * 128 * 128:
+
+    MP = triton.next_power_of_2(M)
+    BLOCK_SIZE_M = max(min(MP, 64), 16)
+    
+    if BLOCK_SIZE_M == 16:
+        if N <= 32 * 128:
+            SPLIT_COUNT = 4
+            BLOCK_SIZE_N = 16
+        else:
+            SPLIT_COUNT = 1
+            BLOCK_SIZE_N = 32
+        num_warps = 2
+    elif BLOCK_SIZE_M == 32:
         SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 64
-        BLOCK_SIZE_N = 128
-        num_warps = 4
-    elif M * N >= 32 * 128 * 128:
-        SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 32
-        BLOCK_SIZE_N = 128
-        num_warps = 4
-    elif M * N >= 16 * 64 * 128:
-        SPLIT_COUNT = 1
-        BLOCK_SIZE_M = 16
         BLOCK_SIZE_N = 64
-        num_warps = 2
+        num_warps = 4
     else:
-        SPLIT_COUNT = 4
-        BLOCK_SIZE_M = 16
-        BLOCK_SIZE_N = 16
-        num_warps = 2
+        SPLIT_COUNT = 1
+        BLOCK_SIZE_N = 128
+        num_warps = 4
+
     BLOCK_SIZE_K = 128
     grid = (SPLIT_COUNT,
             triton.cdiv(M, BLOCK_SIZE_M),
@@ -309,4 +314,108 @@ def triton_split_tile_block_fp8_gemm(a: torch.Tensor,
                                      num_stages=num_stages,
                                      num_warps=num_warps,
                                      )
+    return c
+
+
+
+@triton.jit
+def _compute_pid(tile_id, num_pid_in_group, num_pid_m, GROUP_SIZE_M):
+    group_id = tile_id // num_pid_in_group
+    first_pid_m = group_id * GROUP_SIZE_M
+    group_size_m = min(num_pid_m - first_pid_m, GROUP_SIZE_M)
+    pid_m = first_pid_m + (tile_id % group_size_m)
+    pid_n = (tile_id % num_pid_in_group) // group_size_m
+    return pid_m, pid_n
+
+
+@triton.jit
+def tma_persistent_matmul_kernel(
+        a_desc,
+        b_desc,
+        c_desc,
+        M,
+        N,
+        K,
+        BLOCK_SIZE_M: tl.constexpr,
+        BLOCK_SIZE_N: tl.constexpr,
+        BLOCK_SIZE_K: tl.constexpr,
+        GROUP_SIZE_M: tl.constexpr,
+        SM: tl.constexpr, ):
+    start_pid = tl.program_id(axis=0)
+    num_pid_m = tl.cdiv(M, BLOCK_SIZE_M)
+    num_pid_n = tl.cdiv(N, BLOCK_SIZE_N)
+    k_tiles = tl.cdiv(K, BLOCK_SIZE_K)
+    num_tiles = num_pid_m * num_pid_n
+
+    tid_c = start_pid - SM
+    num_pid_in_group = GROUP_SIZE_M * num_pid_n
+
+    for tid in tl.range(start_pid, num_tiles, SM, flatten=True):
+        pid_m, pid_n = _compute_pid(tid, num_pid_in_group, num_pid_m,
+                                    GROUP_SIZE_M)
+        offs_a = pid_m * BLOCK_SIZE_M
+        offs_b = pid_n * BLOCK_SIZE_N
+
+        accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+        for k in range(k_tiles):
+            offs_k = k * BLOCK_SIZE_K
+            a = a_desc.load([offs_a, offs_k])
+            b = b_desc.load([offs_b, offs_k])
+            accumulator = tl.dot(a, b.T, accumulator)
+
+        tid_c += SM
+        pid_m, pid_n = _compute_pid(tid_c, num_pid_in_group, num_pid_m, GROUP_SIZE_M)
+        offs_a_acc = pid_m * BLOCK_SIZE_M
+        offs_b_acc = pid_n * BLOCK_SIZE_N
+
+        acc = tl.reshape(accumulator, (BLOCK_SIZE_M, 2, BLOCK_SIZE_N // 2))
+        acc = tl.permute(acc, (0, 2, 1))
+        acc0, acc1 = tl.split(acc)
+        c_desc.store([offs_a_acc, offs_b_acc], acc0)
+        c_desc.store([offs_a_acc, offs_b_acc + BLOCK_SIZE_N // 2], acc1)
+
+
+def triton_tma_persistent_matmul(a, b):
+    M, K = a.shape
+    N, K = b.shape
+
+    device = a.device
+    c = torch.empty((M, N), device=device, dtype=torch.float32)
+
+    SM = torch.cuda.get_device_properties(a.device).multi_processor_count
+
+    MP = triton.next_power_of_2(M)
+    BLOCK_M = max(min(MP, 128), 16)
+    BLOCK_K = 128
+    BLOCK_N = 128
+    GROUP_SIZE_M = 8
+
+    a_desc = triton.tools.tensor_descriptor.TensorDescriptor(a, a.shape,
+                                                             a.stride(),
+                                                             [BLOCK_M, BLOCK_K])
+    b_desc = triton.tools.tensor_descriptor.TensorDescriptor(b, b.shape,
+                                                             b.stride(),
+                                                             [BLOCK_N, BLOCK_K])
+    c_desc = triton.tools.tensor_descriptor.TensorDescriptor(c, c.shape,
+                                                             c.stride(),
+                                                             [BLOCK_M,
+                                                              BLOCK_N // 2])
+
+    def grid(META):
+        nonlocal a_desc, b_desc, c_desc
+        return (min(SM,
+                    triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N), ),)
+
+    tma_persistent_matmul_kernel[grid](
+        a_desc,
+        b_desc,
+        c_desc,
+        M,
+        N,
+        K,
+        BLOCK_M,
+        BLOCK_K,
+        BLOCK_N,
+        GROUP_SIZE_M,
+        SM=SM)
     return c
--- tests/infer/test_gemm.py
+++ tests/infer/test_gemm.py
@@ -7,8 +7,8 @@ import torch
 
 from linghe.infer.gemm import (triton_split_fp32_gemm,
                                triton_tile_block_fp8_gemm,
-                               triton_split_tile_block_fp8_gemm)
-from linghe.gemm.fp32_gemm import triton_tma_persistent_matmul
+                               triton_split_tile_block_fp8_gemm,
+                               triton_tma_persistent_matmul)
 from linghe.tools.benchmark import benchmark_func
 from linghe.tools.check import output_check
 from linghe.tools.util import (torch_group_quant,
@@ -45,19 +45,23 @@ def test_fp32_matmul(M=2048, N=256, K=8192, bench=False):
     if bench:
         ref_bytes = M * K * 6 + N * K * 6 + M * N * 4
         ref_flops = 2 * M * N * K
-        ref_time = benchmark_func(torch_fp32_matmul, x, w.float(),
+        ref_time = benchmark_func(torch_fp16_matmul, x, w,
                                   ref_bytes=ref_bytes,
                                   ref_flops=ref_flops)
+        benchmark_func(torch_fp32_matmul, x, w.float(),
+                                  ref_bytes=ref_bytes,
+                                  ref_flops=ref_flops,
+                                  ref_time=ref_time)
         benchmark_func(triton_split_fp32_gemm, x, w,
                        ref_bytes=ref_bytes,
                        ref_flops=ref_flops,
                        ref_time=ref_time,
-                       n_profile=10)
+                       n_profile=0)
         benchmark_func(triton_tma_persistent_matmul, x, w,
                        ref_bytes=ref_bytes,
                        ref_flops=ref_flops,
                        ref_time=ref_time,
-                       n_profile=10)
+                       n_profile=0)
 
 
 def test_fp8_matmul(M=2048, N=1024, K=8192, bench=False):
@@ -104,6 +108,7 @@ def test_fp8_matmul(M=2048, N=1024, K=8192, bench=False):
                        ref_time=ref_time,
                        n_profile=10)
 
+
 if __name__ == '__main__':
     test_fp32_matmul(M=4096, N=256, K=8192, bench=True)
     test_fp32_matmul(M=16384, N=256, K=2048, bench=True)
@@ -112,6 +117,7 @@ if __name__ == '__main__':
     test_fp32_matmul(M=2048, N=157184, K=2048, bench=True)
     test_fp32_matmul(M=2048-32, N=157184, K=4096, bench=True)
     test_fp32_matmul(M=2048-1, N=157184, K=8192, bench=True)
+    test_fp32_matmul(M=4, N=157184, K=8192, bench=True)
 
     test_fp32_matmul(M=128, N=256, K=8192, bench=True)
     test_fp32_matmul(M=4, N=256, K=4096, bench=True)
