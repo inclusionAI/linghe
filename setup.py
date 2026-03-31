4fe88b6c2bba477c47164f2b9686c887b3339242
nanxiao.zy <nanxiao.zy@antgroup.com>
2026-03-31 11:31:00 +0800

    use 0.3.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="linghe",
-    version="0.3.8",
+    version="0.3.9",
     license="MIT",
     license_files=("LICENSE",),
     description="LLM traning kernels",
